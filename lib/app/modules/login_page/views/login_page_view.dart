105da5c824ee8acb377919b3a8b55a16c88b730f
khilan <khilanvithani0080@gmail.com>
2022-08-04 17:59:21 +0530

    test commit.

--- lib/app/modules/login_page/views/login_page_view.dart
+++ lib/app/modules/login_page/views/login_page_view.dart
@@ -66,7 +66,7 @@ class LoginPageView extends GetView<LoginPageController> {
               Space.height(10),
               Container(
                 child: Text(
-                  "Please enter your mail and password to continue",
+                  "Pleasee enter your mail and password to continue",
                   style: TextStyle(
                     color: Colors.black,
                     fontSize: AppSize.size20,
